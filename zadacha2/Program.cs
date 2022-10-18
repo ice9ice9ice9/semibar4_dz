bdd09c4243e09496a91a63e7e49f199d33cbe6e4
Айси <IceIX@MacBook-Pro-Filipp.local>
2022-10-18 13:16:35 +0300

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12 */
+
+Console.WriteLine("Введите число: "); 
+string num = Console.ReadLine();
+
+int count = num.Length;
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine(num[i]);
+}
+
+
